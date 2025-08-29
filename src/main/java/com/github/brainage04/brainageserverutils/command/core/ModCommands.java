e73895bc55eaf88ea2a056cf43a21e101bf5743e
brainage04 <thomasmcmah21@gmail.com>
2025-08-29 12:50:01 +1000

    added example command back

--- src/main/java/com/github/brainage04/brainageserverutils/command/core/ModCommands.java
+++ src/main/java/com/github/brainage04/brainageserverutils/command/core/ModCommands.java
@@ -1,11 +1,12 @@
 package com.github.brainage04.brainageserverutils.command.core;
 
+import com.github.brainage04.brainageserverutils.command.ExampleCommand;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class ModCommands {
     public static void initialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, access, environment) -> {
-
+            ExampleCommand.initialize(dispatcher);
         });
     }
 }
